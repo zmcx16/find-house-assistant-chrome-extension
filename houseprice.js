0f297c6c2cd9b5ccdd4e241aa1a7dd2ee0aa438b
zmcx16 <zmcx16@gmail.com>
2020-02-10 04:37:27 +0800

    Init repo

---
+++ houseprice.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    if (document.getElementsByClassName("search_box_wrap")[0].innerHTML.indexOf("house-hidden-ext-panel") === -1) {
+        document.getElementsByClassName("search_box_wrap")[0].innerHTML += '<div id="house-hidden-ext-panel"><button id="house-hidden-show-all" type="button"">Show All</button><button id="house-hidden-hidden-all" type="button"">Hidden All</button></div>';
+    }
+};
