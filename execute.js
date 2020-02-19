3503a3087ce30d789ee1dc90581b805ecf4113d8
zmcx16 <zmcx16@gmail.com>
2020-02-20 00:22:39 +0800

    implement popup function

--- execute.js
+++ execute.js
@@ -1,11 +1,48 @@
 const extensionID = "mkcilniegejnlgmabdcoiglhekaekfeh";
 var storage_data = { 'show_hidden': {}, 'filter_address': '', 'bookmark':{} };
+var text_dict = {
+  addStar: '追蹤',
+  cancelStar: '取消',
+  showAll: '顯示所有',
+  hiddenAll: '隱藏所有',
+  show: '展開',
+  hidden: '隱藏',
+  filterAddrKeyword: '去除地址關鍵字:',
+  save: '儲存',
+  showStarList: '展開追蹤清單',
+  hiddenStarList: '隱藏追蹤清單',
+  display: '顯示'
+};
 
 var ui_loading = false;
 
+function clearStorageData(dict_key, clear_time_length){
+  
+  var del_list = [];
+  var now = Math.floor(new Date().getTime() / 1000);
+
+  Object.keys(storage_data[dict_key]).forEach(function (key) {
+    if (now - storage_data[dict_key][key].last_time > clear_time_length) {
+      del_list.push(key);
+    }
+  });
+
+  for (var i = 0; i < del_list.length; i++) {
+    delete storage_data[dict_key][del_list[i]];
+  }
+}
+
 const clearData = (data) => {
-  storage_data = { 'show_hidden': {}, 'filter_address': '', 'bookmark': {} };
-  console.log('clear');
+  
+  var clear_time_length = data.days * 86400;
+  clearStorageData('show_hidden', clear_time_length);
+  clearStorageData('bookmark', clear_time_length);
+
+  saveStorage(function() {
+    console.log('do clear');
+    location.reload();
+  });
+  
 };
 
 const onMessage = (message) => {
@@ -30,7 +67,6 @@ function init() {
     initWithDom();
 
     if (Object.keys(stor_data).length > 0) {
-      console.log(stor_data)
       storage_data = {...stor_data["data"]};
       if (!('show_hidden' in storage_data))
         storage_data['show_hidden'] = {};
@@ -40,6 +76,19 @@ function init() {
 
       if (!('bookmark' in storage_data))
         storage_data['bookmark'] = {};
+
+      //remove redundant data
+      var del_show_hidden_id_list = [];
+      Object.keys(storage_data['show_hidden']).forEach(function (key) {
+        if (storage_data['show_hidden'][key].display) {
+          del_show_hidden_id_list.push(key);
+        }
+      });
+      for (var i = 0; i < del_show_hidden_id_list.length; i++) {
+        delete storage_data['show_hidden'][del_show_hidden_id_list[i]];
+      }
+
+      console.log(storage_data);
     }
 
     // update UI
@@ -54,14 +103,15 @@ function initWithDom(){
 
   // add extend panel
   if ($("#vue-container")[0].innerHTML.indexOf("ext-panel") === -1) {
-    var ext_panel = '<div id="ext-panel">' +
+    var ext_panel = 
+    '<div id="ext-panel">' +
       '  <div id="ext-panel-title">Extend Panel</div>' +
-      '  <button id="show-all-btn" type="button">顯示所有</button>' +
-      '  <button id="hidden-all-btn" type="button">隱藏所有</button>' +
-      '  <a href="javascript:void(0);" id="show-detail-btn" class="hyper-link">展開</a>' +
-      '  <span class="filter-address">去除地址關鍵字:</span><input type="text" id="filter-address-input" placeholder=" OO路;XX巷" name="filter-address-input">' +
-      '  <button id="save-filter-address-btn" type="button">儲存</button>' +
-      '  <a href="javascript:void(0);" id="star-detail-btn" class="hyper-link">展開追蹤清單</a>' +
+      '  <button id="show-all-btn" type="button">' + text_dict.showAll + '</button>' +
+      '  <button id="hidden-all-btn" type="button">' + text_dict.hiddenAll + '</button>' +
+      '  <a href="javascript:void(0);" id="show-detail-btn" class="hyper-link">' + text_dict.show + '</a>' +
+      '  <span class="filter-address">' + text_dict.filterAddrKeyword + '</span><input type="text" id="filter-address-input" placeholder=" OO路;XX巷" name="filter-address-input">' +
+      '  <button id="save-filter-address-btn" type="button">' + text_dict.save + '</button>' +
+      '  <a href="javascript:void(0);" id="star-detail-btn" class="hyper-link">' + text_dict.showStarList + '</a>' +
       '  <div id="star-detail" style="display:none;"></div>' +
       '  <div id="hidden-detail" style="display:none;"></div>' +
       '</div>';
@@ -72,12 +122,12 @@ function initWithDom(){
       if ($(this).hasClass('active')) {
         $(this).removeClass('active');
         $('#hidden-detail').css('display', 'none');
-        $('#show-detail-btn')[0].innerText = '展開';
+        $('#show-detail-btn')[0].innerText = text_dict.show;
       }
       else {
         $(this).addClass('active');
         $('#hidden-detail').css('display', 'block');
-        $('#show-detail-btn')[0].innerText = '隱藏';
+        $('#show-detail-btn')[0].innerText = text_dict.hidden;
       }
     });
 
@@ -85,12 +135,12 @@ function initWithDom(){
       if ($(this).hasClass('active')) {
         $(this).removeClass('active');
         $('#star-detail').css('display', 'none');
-        $('#star-detail-btn')[0].innerText = '展開追蹤清單';
+        $('#star-detail-btn')[0].innerText = text_dict.showStarList;
       }
       else {
         $(this).addClass('active');
         $('#star-detail').css('display', 'block');
-        $('#star-detail-btn')[0].innerText = '隱藏追蹤清單';
+        $('#star-detail-btn')[0].innerText = text_dict.hiddenStarList;
       }
     });
 
@@ -113,7 +163,6 @@ function initWithDom(){
       if (ui_loading === false){
         ui_loading = true;
         window.setTimeout((() => {
-          console.log('aaa');
           updateUIContentChange()
           ui_loading = false;
         }), 300);
@@ -124,7 +173,9 @@ function initWithDom(){
     // save filter address
     $('#save-filter-address-btn').click(function () {
       storage_data['filter_address'] = $('#filter-address-input')[0].value;
-      saveStorage();
+      saveStorage(function () {
+        console.log('do save filter address');
+      });
       setCommonUI();
     });
 
@@ -145,9 +196,10 @@ function resetExtendPanel(){
 }
 
 // local function 
-function saveStorage() {
+function saveStorage(callback) {
 
   chrome.storage.local.set({ "data": storage_data }, function () {
+    callback();
     console.log("save completed");
   });
 }
@@ -187,7 +239,7 @@ function setShowHiddenTargets(data_list, val) {
   for (var i = 0; i < data_list.length; i++) {
     storage_data['show_hidden'][data_list[i]] = {
       display: val,
-      last_time: new Date().toLocaleString()
+      last_time: Math.floor(new Date().getTime() / 1000)
     };
   }
 }
@@ -243,7 +295,7 @@ function setStarUI() {
           '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">'+
             '<path id="unstar-icon-' + key + '-1" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" style="fill: gold;"/>' + 
           '</svg>' + 
-          '<div class="text-label" id="unstar-' + key + '-btn-txt">取消</div>' +
+          '<div class="text-label" id="unstar-' + key + '-btn-txt">' + text_dict.cancelStar + '</div>' +
         '</div>' +
         '<a href="'+base_link + key + '" target="_blank" class="hyper-link">' + storage_data['bookmark'][key].desc1 + '</a><a href="'+base_link + key + '" target="_blank" class="hyper-link">' + storage_data['bookmark'][key].desc2 + '</a><a href="'+base_link + key + '" target="_blank" class="hyper-link">' + storage_data['bookmark'][key].desc3 + '</a>' +
       '</div>';
@@ -251,19 +303,23 @@ function setStarUI() {
     star_list.push(key);
   });
 
+  $('.unstar-btn').unbind("click");
+  $('.unstar-btn').click(function () {
+    var unstar_btn = $(this)[0];
+    doStar(unstar_btn.id.replace('unstar-', ''), false, false);
+  });
+
   $(".star-btn").each((index, value) => {
-    console.log(value);
-    console.log($(value)[0].id);
     var id_key = $(value)[0].id.replace('star-', '');
     if (star_list.includes(id_key)){
       $('#star-icon-' + id_key + '-0').css('display', 'none');
       $('#star-icon-' + id_key + '-1').css('display', 'block');
-      $('#star-' + id_key + '-btn-txt')[0].textContent = "取消";
+      $('#star-' + id_key + '-btn-txt')[0].textContent = text_dict.cancelStar;
     }
     else{
       $('#star-icon-' + id_key + '-1').css('display', 'none');
       $('#star-icon-' + id_key + '-0').css('display', 'block');
-      $('#star-' + id_key + '-btn-txt')[0].textContent = "追蹤";
+      $('#star-' + id_key + '-btn-txt')[0].textContent = text_dict.addStar;
     }
   });
 
@@ -280,7 +336,9 @@ function doShow(data) {
 
   setShowHiddenTargets(getIDList(data), true);
   setCommonUI();
-  saveStorage();
+  saveStorage(function () {
+    console.log('save show change');
+  });
 }
 
 function showAll() {
@@ -292,7 +350,9 @@ function doHidden(data) {
 
   setShowHiddenTargets(getIDList(data), false);
   setCommonUI();
-  saveStorage();
+  saveStorage(function () {
+    console.log('save hidden change');
+  });
 }
 
 function hiddenAll() {
@@ -300,18 +360,24 @@ function hiddenAll() {
   doHidden($(".object_con_box.list_con")[0].children);
 }
 
-function setStarTargets(data, val){
+function setStarTargets(data, val, isHtml){
 
   var id;
-  if (val){
+  if (isHtml){
     id = getIDList(data);
     var show_item_child = $('#show-' + id + '-item')[0].children;
-    if (!(id in storage_data['bookmark'])) {
-      storage_data['bookmark'][id] = {
-        desc1: show_item_child[1].innerText,
-        desc2: show_item_child[2].innerText,
-        desc3: show_item_child[3].innerText,
-      };
+    if (val){
+      if (!(id in storage_data['bookmark'])) {
+        storage_data['bookmark'][id] = {
+          desc1: show_item_child[1].innerText,
+          desc2: show_item_child[2].innerText,
+          desc3: show_item_child[3].innerText,
+          last_time: Math.floor(new Date().getTime() / 1000)
+        };
+      }
+    }
+    else{
+      delete storage_data['bookmark'][id];
     }
   }
   else{
@@ -320,11 +386,13 @@ function setStarTargets(data, val){
   }
 }
 
-function doStar(data, val){
+function doStar(data, val, isHtml){
 
-  setStarTargets(data, val);
-  setCommonUI();
-  saveStorage();
+  setStarTargets(data, val, isHtml);
+  setStarUI();
+  saveStorage(function () {
+    console.log('save doStar change');
+  });
 }
 
 function updateUIContentChange() {
@@ -332,7 +400,7 @@ function updateUIContentChange() {
   var data = $(".object_con_box.list_con")[0].children;
   for (var j = 0; j < data.length; j++) {
     var tgt_element = data[j].children[1];
-    if (tgt_element != null && tgt_element.innerHTML.indexOf('div class="icon-text-btn"') === -1 && tgt_element.innerHTML.indexOf('title_list') != -1){
+    if (tgt_element != null && tgt_element.innerHTML.indexOf('icon-text-btn') === -1 && tgt_element.innerHTML.indexOf('title_list') != -1){
       
       // show button
       var obj_desc1, obj_desc2, obj_desc3;
@@ -346,7 +414,7 @@ function updateUIContentChange() {
           '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24">'+
             '<path d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" />'+
           '</svg>' + 
-          '<div class="text-label">顯示</div>' + 
+          '<div class="text-label">' + text_dict.display + '</div>' + 
         '</div>' + 
         '<div>' + obj_desc1 + '</div><div>' + obj_desc2 + '</div><div>' + obj_desc3 + '</div>'+
       '</div>';
@@ -365,7 +433,7 @@ function updateUIContentChange() {
           '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24">'+
             '<path d="M11.83,9L15,12.16C15,12.11 15,12.05 15,12A3,3 0 0,0 12,9C11.94,9 11.89,9 11.83,9M7.53,9.8L9.08,11.35C9.03,11.56 9,11.77 9,12A3,3 0 0,0 12,15C12.22,15 12.44,14.97 12.65,14.92L14.2,16.47C13.53,16.8 12.79,17 12,17A5,5 0 0,1 7,12C7,11.21 7.2,10.47 7.53,9.8M2,4.27L4.28,6.55L4.73,7C3.08,8.3 1.78,10 1,12C2.73,16.39 7,19.5 12,19.5C13.55,19.5 15.03,19.2 16.38,18.66L16.81,19.08L19.73,22L21,20.73L3.27,3M12,7A5,5 0 0,1 17,12C17,12.64 16.87,13.26 16.64,13.82L19.57,16.75C21.07,15.5 22.27,13.86 23,12C21.27,7.61 17,4.5 12,4.5C10.6,4.5 9.26,4.75 8,5.2L10.17,7.35C10.74,7.13 11.35,7 12,7Z" />'+
           '</svg>' + 
-          '<div class="text-label">隱藏</div>' + 
+          '<div class="text-label">' + text_dict.hidden + '</div>' + 
         '</div>' + 
         tgt_element.innerHTML;
 
@@ -387,20 +455,15 @@ function updateUIContentChange() {
 
   $('.star-btn').unbind("click");
   $('.star-btn').click(function () {
-    if ($(this).hasClass('starring')) {
-      $(this).removeClass('starring');
-      doStar([$(this).parent().parent()[0]], false);
+    var id_key = $(this)[0].id.replace('star-', '');
+    if ($('#star-' + id_key + '-btn-txt')[0].textContent == text_dict.cancelStar) {
+      $('#star-' + id_key + '-btn-txt')[0].textContent = text_dict.addStar;
+      doStar([$(this).parent().parent()[0]], false, true);
     }
     else {
-      $(this).addClass('starring');
-      doStar([$(this).parent().parent()[0]], true);
+      $('#star-' + id_key + '-btn-txt')[0].textContent = text_dict.cancelStar;
+      doStar([$(this).parent().parent()[0]], true, true);
     }
-  });
-
-  $('.unstar-btn').unbind("click");
-  $('.unstar-btn').click(function () {
-    var unstar_btn = $(this)[0];
-    doStar(unstar_btn.id.replace('unstar-', ''), false);
   });
 }
 
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 var getSelectedTab = (tab) => {
   var tabId = tab.id;
   var sendMessage = (messageObj) => chrome.tabs.sendMessage(tabId, messageObj);
-  document.getElementById('clear-data').addEventListener('click', () => sendMessage({ action: 'CLEAR', data: {'days':30 } }));
+  document.getElementById('clear-data-btn').addEventListener('click', () => sendMessage({ action: 'CLEAR', data: { 'days': parseFloat(document.getElementById('clear-data-input').value) } }));
 }
 chrome.tabs.getSelected(null, getSelectedTab);
